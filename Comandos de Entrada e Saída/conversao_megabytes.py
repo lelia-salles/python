53f0d05241d55f8dc95c58f3ce98cc49e643da4a
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-27 12:20:49 -0300

    added conversao_gigab

---
+++ Comandos de Entrada e Saída/conversao_megabytes.py
@@ -0,0 +1,17 @@
+'''Suponha que você está considerando o espaço de armazenamento necessário para armazenar uma coleção de fotos. 
+Digamos que cada foto tenha um tamanho médio de 2 megabytes (MB). 
+Agora, você deseja calcular quantas fotos caberiam em um dispositivo de armazenamento que tem capacidade de 64 gigabytes (GB).
+
+Primeiro, converta 64 GB para megabytes (MB):
+
+64  GB = 64 × 1024  MB = 65536 MB
+64 GB=64×1024 MB=65536 MB
+
+Em seguida, calcule quantas fotos podem ser armazenadas dividindo o espaço total pelo tamanho médio de cada foto:
+
+65536 MB / 2 MB por foto = 32768 fotos
+2 MB por foto
+65536 MB
+​
+ =32768 fotos
+Para determinar quantos gigabytes (GB) seriam necessários para armazenar o dobro desse número de fotos, você pode usar logaritmos de base 2 para determinar o número necessário de gigabytes:'''
