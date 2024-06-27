7b6c47f891f20ed6c2351a12d767299f77def94f
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-27 11:57:59 -0300

    added converter segundos

---
+++ Comandos de Entrada e Saída/conta_segundos.py
@@ -0,0 +1,9 @@
+segundos = input('Digite a quantidade de segundos que quer converter: ')
+total_segundos = int(segundos)
+
+horas = total_segundos // 3600 # só a parte inteira
+segundos_restantes = total_segundos % 3600 # o resto da divisãp
+minutos = segundos_restantes // 60 # divisão inteira
+segundos_restantes_final = segundos_restantes % 60 # restp da divisão
+
+print (horas, 'horas', minutos, 'minutos e ', segundos_restantes_final, 'segundos')
