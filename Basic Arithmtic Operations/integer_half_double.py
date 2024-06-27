138fa19e26ae902328ff5fd8aec1855ad96bd761
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-27 10:15:08 -0300

    updated basic math operations

---
+++ Basic Arithmtic Operations/integer_half_double.py
@@ -0,0 +1,5 @@
+number = int(input('Enter a number: '))
+half = number / 2
+double = number * 2
+
+print(' Half of ', number, 'is: ', half, ', and its double is: ', double)
---
+++ Basic Arithmtic Operations/integer_predeccessor_successor.py
@@ -0,0 +1,6 @@
+number = int(input('Enter a number: '))
+sucessor = number + 1
+predecessor = number - 1
+
+print(" Sucessor is : ", sucessor, ', and predecessor is : ', predecessor)
+ 
