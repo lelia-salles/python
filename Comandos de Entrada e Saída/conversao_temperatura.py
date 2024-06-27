15b7aa386bec2f86a199c67540439bd85b66911a
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-27 11:50:01 -0300

    added conversao de temperatura

---
+++ Comandos de Entrada e Saída/conversao_temperatura.py
@@ -0,0 +1,14 @@
+F = int(input('Digite a temperatura em Farenheit: '))
+F = 78 # F é temperatura para Farenheit
+teperaturaCelsius = (F-32) * 5/9
+
+print('Temperaturaa em celsius é: ', teperaturaCelsiusemperaturaCelsius)
+
+#ou
+
+F1 = input('Digite a temperatura em Farenheit: ')
+F1 = 78 # F1 é temperatura para Farenheit
+teperaturaCelsius = (float(F1-32) )* 5/9
+
+print('Temperaturaa em celsius é: ', teperaturaCelsiusemperaturaCelsius)
+
