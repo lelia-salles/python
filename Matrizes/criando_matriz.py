91cf9a3425e15513c41ef4f5733b82efb867286c
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-04 15:42:45 +0000

    updated

--- Matrizes/criando_matriz.py
+++ Matrizes/criando_matriz.py
@@ -23,4 +23,37 @@ print(matriz_pessoa[0][2])
 matriz_pessoa[0][1] = 25
 print(matriz_pessoa)
 
+# Exemplo 3
+# criando uma matriz de números
+matriz_numeros = [[0,1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]]
+
+#imprime grupo de elementos da linha 0
+print('\nGrupo de números da linha 0: ',matriz_numeros[0])
+
+#imprime grupo de elementos da linha 1
+print('\nGrupo de números da linha 1: ',matriz_numeros[1])
+
+#imprime grupo de elementos da linha 2
+print('\nGrupo de números da linha 2: ',matriz_numeros[2])
+
+#acessando intervalo de linhas
+print('\nGrupo de números da linha 0 e 1: ',matriz_numeros[0:2])
+
+#acessando intervalo de colunas
+print('\nGrupo de números da coluna 0 e 1: ',matriz_numeros[:,0:2])
+
+#acessando intervalo de linhas e colunas
+print('\nGrupo de números da linha 0 e 1 e da coluna 0 e 1: ',matriz_numeros[0:2,0:2]) 
+
+#modificando uma linha
+matriz_numeros[0] = [1, 2, 3, 4]
+print('\nGrupo de números da linha 0: ',matriz_numeros[0])
+
+#modificando uma coluna
+matriz_numeros[:,0] = [1, 2, 3, 4]
+print('\nGrupo de números da coluna 0: ',matriz_numeros[:,0])
+
+#modificando uma linha e coluna
+matriz_numeros[0,0] = 1
+print('\nGrupo de números da linha 0 e coluna 0: ',matriz_numeros[0,0])
 
