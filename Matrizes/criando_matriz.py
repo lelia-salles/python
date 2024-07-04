baf3a5ac15a4b6b1f5ba7c736b66359c7c27f063
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-04 15:26:22 +0000

    added criando matriz

---
+++ Matrizes/criando_matriz.py
@@ -0,0 +1,19 @@
+"""
+matriz é uma lista de listas
+"""
+matriz = [[1, 2, 3], [4, 5, 6]]
+
+print(matriz[0])
+print(matriz[0][0])
+print(matriz[1][1])
+
+
+
+matriz_pessoa = [['João', 22, 345768425485],['Maria', 44, 323456789012],['Pedro', 33, 345678901234]]
+print(matriz_pessoa)
+print(matriz_pessoa[0])
+print(matriz_pessoa[1])
+print(matriz_pessoa[2])
+print(matriz_pessoa[0][0])
+print(matriz_pessoa[0][1])
+print(matriz_pessoa[0][2])
