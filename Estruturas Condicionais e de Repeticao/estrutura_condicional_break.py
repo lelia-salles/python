2d8fef8bdd544fa606fc04d4f9b59ce7e29ee4a4
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-05 13:35:56 +0000

    added

---
+++ Estruturas Condicionais e de Repeticao/estrutura_condicional_break.py
@@ -0,0 +1,19 @@
+#original trilha python basico
+while True:
+    numero = int(input("Informe um número: "))
+
+    if numero == 10:
+        break
+
+    if numero % 2 == 0:
+        continue
+
+    print(numero)
+
+
+# for numero in range(100):
+
+#     if numero % 2 == 0:
+#         continue
+
+#     print(numero, end=" ")
