2addd25774c2921e587516521de835abe1bf2656
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-11 12:57:02 +0000

    updated

---
+++ Matrizes/operadores_de_matrizes.py
@@ -0,0 +1,19 @@
+# matriz 2x3 com valores numéricos
+# '+' adiciona o conteúdo de uma matriz à outra
+
+num = [[1, 2, 3], [4, 5, 6]]
+num + num
+num + [[7, 8, 9]]
+
+# matriz 2x2 com valores literais
+mat = [['A', 'B'], ['C', 'D']]
+mat + mat
+mat + [['E', 'F']]
+
+# ´*´ aumenta as linhas das matrizes x vezes. Multiplica a quantidade de listas
+# dentro da matriz por um valor numérico desejado.
+
+num = [[1, 2, 3], [4, 5, 6]]
+num * 3
+teste = [[0]] * 10
+teste_2 = [[2, 3]] * 2
