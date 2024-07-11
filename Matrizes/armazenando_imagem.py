822c495982cc69beb5cd7a826366940f5e2ffd0b
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-11 14:26:56 +0000

    updated

--- Matrizes/armazenando_imagem.py
+++ Matrizes/armazenando_imagem.py
@@ -1,2 +1,11 @@
 # importação da biblioteca
 from matplotlib.image import imread
+# leitura da imagm com a função imread()
+imagem = imread('/workspaces/python-basico/Matrizes/assets/waldir azevedo criança com a flauta.jpg')
+# exibição da imagem
+print("altura em pixels: ", end= " ")
+print(imagem.shape[0]) # altura da imagem
+print("largura em pixels: ", end= " ")
+print(imagem.shape[1])
+print("canais de cor: ", imagem.shape[2]) # outra forma de escrever
+
