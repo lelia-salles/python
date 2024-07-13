e5ea09357f94e87858f89d2a6716ddbe0d9ba3ac
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-13 18:26:12 +0000

    add

---
+++ Classe String/strings_triplas.py
@@ -0,0 +1,19 @@
+#exemplos de string tripas 
+
+#string tripla com aspas simples
+texto = '''
+Olá, mundo!
+Este é um texto de várias linhas.
+Ele pode ter qualquer tamanho.
+'''
+
+print(texto)
+
+#string tripla com aspas duplas
+texto = """
+Olá, mundo!
+Este é um texto de várias linhas.
+Ele pode ter qualquer tamanho.
+"""
+
+print(texto)
