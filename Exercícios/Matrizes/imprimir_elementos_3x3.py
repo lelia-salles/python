cb9300333171013cc9cf78dddfb9e451439ca289
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-18 19:51:32 +0000

    updated

--- Exercícios/Matrizes/imprimir_elementos_3x3.py
+++ Exercícios/Matrizes/imprimir_elementos_3x3.py
@@ -2,24 +2,12 @@
 # nas lihas e colunas correspondentes
 # acessar elemento por elemento individualmente
 
-exemplo= [[1,2,3],[4,5,6],[7,8,9]
-]
-
-for i in range(len(exemplo)):
-    for j in range(len(exemplo[i])):
-        print(exemplo[0][0])
-        print(exemplo[0][1])
-        print(exemplo[0][2])
-
-        print()
+exemplo= [[1,2,3],[4,5,6],[7,8,9]]
 
 print('---------Matriz----------')
 print('--------------------------')
-for i in range(len(exemplo)):
-        for col in range(len(exemplo[i])):
-                print( end='\t')
-                print(f'{exemplo[i][col]:}', end='\t')
-                print()
-        #print('|')
-        #print('--------------------------')
-        #print(i)
+for j in range(len(exemplo[0])):  # Itera pelas colunas (0, 1, 2)
+    for i in range(len(exemplo)):  # Itera pelas linhas (0, 1, 2)
+        print(exemplo[i][j], end='\t')  # Imprime o elemento da coluna atual
+    print()  # Pula para a próxima linha após imprimir a coluna
+print('--------------------------')
