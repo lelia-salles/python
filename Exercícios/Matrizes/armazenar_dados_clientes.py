4a0542a12170a3b9b78a22b0f8230630a9883f3a
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-18 20:16:07 +0000

    add, update

---
+++ Exercícios/Matrizes/armazenar_dados_clientes.py
@@ -0,0 +1,10 @@
+# desenvolver um programa para armazenar em uma matriz os seguintes dados de dois clientes (nome da pessoa, telefone e sexo (todos do tipo string)) digitados pelo teclado.
+
+
+nome = input('Digite o nome do cliente: ')
+telefone = input('Digite o telefone do cliente: ')
+sexo = input('Digite o sexo do cliente: ')
+
+matriz = [[nome, telefone, sexo], [nome, telefone, sexo]]
+
+
